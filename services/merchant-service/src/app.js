72cdf1c663c559828929cc9971dbc6b66d4372a0
vladimiruzelac <uzelac92@yahoo.com>
2025-09-05 21:56:12 +0200

    cleanup

---
+++ services/merchant-service/src/app.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json({limit: "1mb"}))
+app.use(express.urlencoded({extended: true}))
+app.use(helmet());
+app.use(cors())
+app.use(hpp)
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else app.use(morgan('tiny'));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to merchant service.")
+})
+
+module.exports = app
