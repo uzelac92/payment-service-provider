70916f20dd5fb0209c3a803cab46f2db97fee42a
vladimiruzelac <uzelac92@yahoo.com>
2025-09-05 17:23:28 +0200

    initial project

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}...`);
+});
