c6205b85e7afe90a4db70840c16078fc1cb2124a
vladimiruzelac <uzelac92@yahoo.com>
2025-09-05 21:51:33 +0200

    cleanup

--- services/merchant-service/src/server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require("./app")
-
-const PORT = process.env.PORT || 4002;
-app.listen(PORT, () => {
-    console.log(`Starting merchant-service on port ${PORT}`);
-})
